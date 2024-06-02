6f858b4f2cafeb6d081a5a8aea1dfdb32808507f
Phuong Hoang <114068267+PhuongHoang134@users.noreply.github.com>
2024-06-02 15:54:05 +1000

    Update category

--- category.php
+++ category.php
@@ -1,63 +1,71 @@
 <?php
 require 'db.php';
-$query_movie = "SELECT m.movie_id, m.title, m.overview, m.movie_image,
-              AVG(r.rating) AS avg_rating, GROUP_CONCAT(g.genre_name) AS genre_name
-              FROM movie m
-              LEFT JOIN rating r on r.movie_id = m.movie_id
-              LEFT JOIN movie_genre mg on mg.movie_id = m.movie_id
-              LEFT JOIN genre g on g.genre_id = mg.genre_id
-              GROUP BY m.movie_id, m.title, m.overview, m.movie_image
-              ORDER BY avg_rating DESC";
 
 $query_genre = "SELECT * FROM Genre";
 $query_country = "SELECT * FROM Country";
 $query_language = "SELECT * FROM Language";
 
+$search_query = isset($_GET['search_query']) ? $_GET['search_query'] : '';
+
+// Construct the main query
+$query_movie = "SELECT m.movie_id, m.title, m.overview, m.movie_image,
+                AVG(r.rating) AS avg_rating, GROUP_CONCAT(g.genre_name) AS genre_name
+                FROM movie m
+                LEFT JOIN rating r ON r.movie_id = m.movie_id
+                LEFT JOIN movie_genre mg ON mg.movie_id = m.movie_id
+                LEFT JOIN genre g ON g.genre_id = mg.genre_id
+                LEFT JOIN movie_language ml ON ml.movie_id = m.movie_id
+                LEFT JOIN language l ON l.language_id = ml.language_id
+                LEFT JOIN production_country pc ON pc.movie_id = m.movie_id
+                LEFT JOIN country c ON c.country_id = pc.country_id";
+
+// Add filtering conditions
+$whereClause = "";
+if (!empty($search_query)) {
+    $whereClause .= " AND m.title LIKE '%$search_query%'";
+}
+if (isset($_GET['genre']) && $_GET['genre'] != '') {
+    $genre_id = $_GET['genre'];
+    $whereClause .= " AND g.genre_id = '$genre_id'";
+}
+if (isset($_GET['countries']) && $_GET['countries'] != '') {
+    $country_id = $_GET['countries'];
+    $whereClause .= " AND c.country_id = '$country_id'";
+}
+if (isset($_GET['languages']) && $_GET['languages'] != '') {
+    $language_id = $_GET['languages'];
+    $whereClause .= " AND l.language_id = '$language_id'";
+}
+
+// Append the WHERE clause if necessary
+if (!empty($whereClause)) {
+    $query_movie .= " WHERE " . ltrim($whereClause, ' AND');
+}
+
+// Group by and order
+$query_movie .= " GROUP BY m.movie_id, m.title, m.overview, m.movie_image
+                  ORDER BY avg_rating DESC";
+
+// Execute the main query
 $stmt1 = $db->prepare($query_movie);
 $stmt1->execute();
 $actionMovies = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-if (!$actionMovies) {
-    echo "Error";
-}
 
+// Execute other queries
 $stmt2 = $db->prepare($query_country);
 $stmt2->execute();
 $countries = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-if (!$countries) {
-    echo "Error";
-}
 
 $stmt3 = $db->prepare($query_language);
 $stmt3->execute();
 $languages = $stmt3->fetchAll(PDO::FETCH_ASSOC);
-if (!$languages) {
-    echo "Error";
-}
 
 $stmt4 = $db->prepare($query_genre);
 $stmt4->execute();
 $genre = $stmt4->fetchAll(PDO::FETCH_ASSOC);
-if (!$genre) {
-    echo "Error";
-}
-
-$whereClause = "";
-if (isset($_GET['genre'])  && $_GET['genre'] != '') {
-    $genre_id = $_GET['genre'];
-    $whereClause .= " g.genre_id = '$genre_id'";
-}
-if (isset($_GET['countries'])  && $_GET['countries'] != '') {
-    $countryId = $_GET['countries'];
-    $whereClause .= ($whereClause == '') ? " WHERE" : " AND";
-    $whereClause .= "country.countryId = '$countryId'";
-}
-if (isset($_GET['languages'])  && $_GET['languages'] != '') {
-    $languagesId = $_GET['languages'];
-    $whereClause .= ($whereClause == '') ? " WHERE" : " AND";
-    $whereClause .= "language.languageId = '$languagesId'";
-}
 
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -84,10 +92,10 @@ if (isset($_GET['languages'])  && $_GET['languages'] != '') {
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <form class="d-flex me-3 w-100" role="search">
+            <form class="d-flex me-3 w-100" role="search" method="GET" action="">
                 <div class="input-group">
                     <span class="input-group-text rounded-start-pill"><i class="fa fa-search"></i></span>
-                    <input type="text" class="form-control" placeholder="Search by movie name" aria-label="Search Movies">
+                    <input type="text" class="form-control" placeholder="Search by movie name" aria-label="Search Movies" name="search_query">
                     <span class="input-group-text rounded-end-circle"><i class="fa fa-sliders"></i></span>
                 </div>
             </form>
@@ -109,102 +117,33 @@ if (isset($_GET['languages'])  && $_GET['languages'] != '') {
 
     </div>
 </nav>
-<!-- Search Bar -->
 
 <!--Category Lists-->
 <div class="container py-3 my-3">
-<h3>All Movies</h3>
-<form class="d-flex py-3 my-3" role="search">
-    <div class="input-group">
-        <span class="input-group-text rounded-start-pill"><i class="fa fa-search"></i></span>
-        <input type="text" class="form-control" placeholder="Search by movie name" aria-label="Search Movies">
-        <span class="input-group-text rounded-end-circle"><i class="fa fa-sliders"></i></span>
-    </div>
-</form>
     <div class="row g-5">
         <div class="col-sm-4">
-            <form id="filterForm" method="GET" action="">
-                <div class="d-flex justify-content-between">
-                    <h3>Filter By</h3>
-                    <button class=" btn-dark btn" onclick="filter()"> Apply Filter</button>
-                </div>
-            <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" disabled data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Language
-                        </button>
-                    </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapsed" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <?php
-                            if (count($genre) > 0) {
-                                foreach ($genre as $row) {
-                                    echo '<div class="form-check">';
-                                    echo '<input class="form-check-input" type="checkbox" name="genres[]" value="'. $row['genre_id'] .'" id="flexCheckDefault">';
-                                    echo '<label class="form-check-label" for="flexCheckDefault">';
-                                    echo $row['genre_name'];
-                                    echo '</label>';
-                                    echo '</div>';
-                                }
-                            } else {
-                                echo "<div>No data found</div>";
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed " disabled type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Country
-                        </button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapsed" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <?php
-                            if (count($countries) > 0) {
-                                foreach ($countries as $row) {
-                                echo '<div class="form-check">';
-                                echo '<input class="form-check-input" type="checkbox" value="'. $row['country_id'] .'" name="countries[]" id="flexCheckDefault">';
-                                echo '<label class="form-check-label" for="flexCheckDefault">';
-                                echo $row['country_name'];
-                                echo '</label>';
-                                echo '</div>';
-                                }
-                            } else {
-                                echo "<div>No data found</div>";
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" disabled data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Language
-                        </button>
-                    </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapsed" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <?php
-                            if (count($languages) > 0) {
-                                foreach ($languages as $row) {
-                                    echo '<div class="form-check">';
-                                    echo '<input class="form-check-input" type="checkbox" name="languages[]" value="'. $row['language_id'] .'" id="flexCheckDefault">';
-                                    echo '<label class="form-check-label" for="flexCheckDefault">';
-                                    echo $row['language_name'];
-                                    echo '</label>';
-                                    echo '</div>';
-                                }
-                            } else {
-                                echo "<div>No data found</div>";
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <form>
+                
+                <!-- New filter dropdowns for genres, languages, and countries -->
+                <select class="form-select mt-2" name="genre">
+                    <option value="">Select Genre</option>
+                    <?php foreach ($genre as $g) { ?>
+                        <option value="<?php echo $g['genre_id']; ?>"><?php echo $g['genre_name']; ?></option>
+                    <?php } ?>
+                </select>
+                <select class="form-select mt-2" name="countries">
+                    <option value="">Select Country</option>
+                    <?php foreach ($countries as $c) { ?>
+                        <option value="<?php echo $c['country_id']; ?>"><?php echo $c['country_name']; ?></option>
+                    <?php } ?>
+                </select>
+                <select class="form-select mt-2" name="languages">
+                    <option value="">Select Language</option>
+                    <?php foreach ($languages as $l) { ?>
+                        <option value="<?php echo $l['language_id']; ?>"><?php echo $l['language_name']; ?></option>
+                    <?php } ?>
+                </select>
+
             </form>
         </div>
         <div class="col-sm-8 row g-3">
@@ -241,57 +180,14 @@ if (isset($_GET['languages'])  && $_GET['languages'] != '') {
 </div>
     </div>
 </div>
-<!--Footer-->
-<div class="footer">
-    <div class="container">
-        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5">
-            <div class="col mb-3">
-                <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-                    <img src="./assets/Logo/FilmVault_purple2-removebg-preview.png" alt="Logo" width="100" height="78" class="bi me-2">
-                </a>
-                <p class="text-light">Your Ultimate Cinematic Companion. Discover, organize, and explore your favorite films with ease. Create personalized watchlists, leave insightful reviews, and connect with fellow movie enthusiasts. Dive into a world of cinema with FilmVault.</p>
-                <p class="text-light">&copy; 2024 FilmVault</p>
-            </div>
+<script>
+    $(document).ready(function(){
+        $('select').change(function(){
+            $('form').submit();
+        });
+    });
+</script>
 
-            <div class="col mb-3">
-
-            </div>
-
-            <div class="col mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">About</a></li>
-                </ul>
-            </div>
-
-            <div class="col mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">About</a></li>
-                </ul>
-            </div>
-
-            <div class="col mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">About</a></li>
-                </ul>
-            </div>
-        </footer>
-    </div>
-</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
