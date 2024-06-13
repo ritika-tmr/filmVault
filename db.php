dc414d89e12be21ac9a531372f2b7408002eb610
Phuong Hoang <114068267+PhuongHoang134@users.noreply.github.com>
2024-06-13 14:26:05 +1000

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=db_22075125', '22075125', 'mydb22075125');
+    $db = new PDO('mysql:host=localhost;dbname=filmVault', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Connection failed: ' . $e->getMessage());
